686f6e6670f072aa4a8f199eabcfe561728c4871
TJ Peden <tj@tjcoding.com>
2020-07-02 00:55:44 -0500

    Fixed formatting.

--- src/application.rs
+++ src/application.rs
@@ -1,70 +1,52 @@
-use piston::input::{
-  ButtonArgs,
-  RenderArgs,
-  UpdateArgs,
-};
-use opengl_graphics::{
-  GlGraphics,
-  Texture,
-  TextureSettings,
-};
-use image::{
-  ImageBuffer,
-  Rgba,
-};
+use image::{ImageBuffer, Rgba};
+use opengl_graphics::{GlGraphics, Texture, TextureSettings};
+use piston::input::{ButtonArgs, RenderArgs, UpdateArgs};
 
 use crate::noise_map::NoiseMap;
 
 pub struct Application {
-  gl: GlGraphics,
-  width: u32,
-  height: u32,
-  noise_map: NoiseMap,
+    gl: GlGraphics,
+    width: u32,
+    height: u32,
+    noise_map: NoiseMap,
 }
 
 impl Application {
-  pub fn new(width: u32, height: u32, gl: GlGraphics) -> Self {
-      let noise_map = NoiseMap::new(width as usize, height as usize, 0.3);
+    pub fn new(width: u32, height: u32, gl: GlGraphics) -> Self {
+        let noise_map = NoiseMap::new(width as usize, height as usize, 0.3);
 
-      Application {
-          gl,
-          width,
-          height,
-          noise_map,
-      }
-  }
+        Application {
+            gl,
+            width,
+            height,
+            noise_map,
+        }
+    }
 
-  pub fn render(&mut self, args: &RenderArgs) {
-      use graphics::*;
-      use crate::colors::*;
+    pub fn render(&mut self, args: &RenderArgs) {
+        use crate::colors::*;
+        use graphics::*;
 
-      let width = self.width;
-      let height = self.height;
-      let noise_map = &self.noise_map;
+        let width = self.width;
+        let height = self.height;
+        let noise_map = &self.noise_map;
 
-      self.gl.draw(
-          args.viewport(),
-          |context, gl| {
-              clear(WHITE, gl);
+        self.gl.draw(args.viewport(), |context, gl| {
+            clear(WHITE, gl);
 
-              let buffer = ImageBuffer::from_fn(
-                  width,
-                  height,
-                  |x, y| {
-                      let value = (255.0 * noise_map[(x, y)]) as u8;
+            let buffer = ImageBuffer::from_fn(width, height, |x, y| {
+                let value = (255.0 * noise_map[(x, y)]) as u8;
 
-                      Rgba([value, value, value, 255])
-                  },
-              );
+                Rgba([value, value, value, 255])
+            });
 
-              let texture = Texture::from_image(&buffer, &TextureSettings::new());
+            let texture = Texture::from_image(&buffer, &TextureSettings::new());
 
-              image(&texture, context.transform, gl);
-          },
-      );
-  }
+            image(&texture, context.transform, gl);
+        });
+    }
 
-  pub fn update(&mut self, _args: &UpdateArgs) {}
+    pub fn update(&mut self, _args: &UpdateArgs) {}
 
-  pub fn handle(&mut self, _args: &ButtonArgs) {}
+    pub fn handle(&mut self, _args: &ButtonArgs) {}
 }
